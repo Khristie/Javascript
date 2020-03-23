4dd2ba94b6a4a8b12f882460708729aafee7f8b3
Khristie <52844682+Khristie@users.noreply.github.com>
2020-03-24 00:58:42 +0100

    another commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var myName= "Christiana Olawumi"
+console.log (myName);
+
+var courses= ["html", "css", "javascript"]
+console.log (courses);
+
+
+for (var x=1; x<=200; x++) {
+    if (x === 1) {
+            console.log(x);
+    }
+    else if (x % 2 === 1) {
+            console.log(x);   
+    }
+    
+}
